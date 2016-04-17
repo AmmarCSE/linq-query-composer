eab4a7330cc8cc818bb1c62e13d316d182af7bee
AmmarCSE <ammar.abu-yasein@farsightsoftware.com>
2016-04-17 03:33:38 -0700

    pre-merge

---
+++ linq-query-composer/DBContext/Model1.Designer.cs
@@ -0,0 +1,4 @@
+﻿// Default code generation is disabled for model 'C:\Users\AmmarW8\Documents\GitHub\linq-query-composer\linq-query-composer\DBContext\Model1.edmx'. 
+// To enable default code generation, change the value of the 'Code Generation Strategy' designer
+// property to an alternate value. This property is available in the Properties Window when the model is
+// open in the designer.
